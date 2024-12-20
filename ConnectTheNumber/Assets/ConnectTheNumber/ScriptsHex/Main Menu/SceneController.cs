8b60811f1f8985bfc4a309243bf345b2f46f10fd
Muhammad Qasim <71691410+fojiphoto@users.noreply.github.com>
2024-12-20 17:13:33 +0500

    version changed

--- ConnectTheNumber/Assets/ConnectTheNumber/ScriptsHex/Main Menu/SceneController.cs
+++ ConnectTheNumber/Assets/ConnectTheNumber/ScriptsHex/Main Menu/SceneController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
-using Unity.VisualScripting;
+//using Unity.VisualScripting;
 
 public class SceneController : MonoBehaviour
 {
